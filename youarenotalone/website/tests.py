161f1ecab306fda5e24d8d71dfb089c11835f168
Oxyo78 <vienne.yohan@gmail.com>
2018-08-14 14:35:18 +0200

    Add user connection test

--- youarenotalone/website/tests.py
+++ youarenotalone/website/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.test import Client
-
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.support.wait import WebDriverWait
 
 class UrlTestCase(TestCase):
     """ URL test """
@@ -10,5 +12,64 @@ class UrlTestCase(TestCase):
 
     def test_url(self):
         home = self.client.get('')
+        logout = self.client.get('logout/')
 
         self.assertEqual(home.status_code, 200)
+        self.assertEqual(logout.status_code, 404)
+
+
+class MySeleniumTests(StaticLiveServerTestCase):
+    """ Try connexion and logout of a user """
+    fixtures = ['user.json']
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = WebDriver()
+        cls.selenium.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    def test_login(self):
+        """ Login & disconnect user """
+        timeout = 2
+        self.selenium.get('%s%s' % (self.live_server_url, ''))
+        self.selenium.find_element_by_id('login').click()
+        username_input = self.selenium.find_element_by_id("usernameInputControl")
+        username_input.send_keys('Paul01')
+        password_input = self.selenium.find_element_by_id("passwordControl")
+        password_input.send_keys('azerty')
+        self.selenium.find_element_by_id('connect').click()
+        WebDriverWait(self.selenium, timeout).until(
+            lambda driver: driver.find_element_by_id('account'))
+        self.selenium.find_element_by_id('logout').click()
+
+    def test_create_user(self):
+        """ Create user """
+        timeout = 2
+        self.selenium.get('%s%s' % (self.live_server_url, ''))
+        self.selenium.find_element_by_id('signin').click()
+        username_input = self.selenium.find_element_by_id("usernameInput")
+        username_input.send_keys('Paul02')
+        password_input = self.selenium.find_element_by_id("emailInput")
+        password_input.send_keys('paul@example.com')
+        password_input = self.selenium.find_element_by_id("passwordInput")
+        password_input.send_keys('azerty')
+        password_input = self.selenium.find_element_by_id("password2Input")
+        password_input.send_keys('azerty')
+        self.selenium.find_element_by_id('subscribebutton').click()
+        self.selenium.find_element_by_id('logout').click()
+
+        """ Login with the new account """
+        self.selenium.find_element_by_id('login').click()
+        username_input = self.selenium.find_element_by_id("usernameInputControl")
+        username_input.send_keys('Paul02')
+        password_input = self.selenium.find_element_by_id("passwordControl")
+        password_input.send_keys('azerty')
+        self.selenium.find_element_by_id('connect').click()
+        WebDriverWait(self.selenium, timeout).until(
+            lambda driver: driver.find_element_by_id('account'))
+        self.selenium.find_element_by_id('logout').click()
--- youarenotalone/youarenotalone/urls.py
+++ youarenotalone/youarenotalone/urls.py
@@ -14,13 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from website import views
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='home'),
-    path('logout/', views.logoutUser, name='logout'),
+    path('', include('website.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
