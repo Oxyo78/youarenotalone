c22a56034f342209c9aadcf7b65dde01ba91e9ee
Oxyo78 <vienne.yohan@gmail.com>
2018-09-02 23:23:37 +0200

    Update Map function ( add full Ajax search & map show)

--- youarenotalone/website/static/website/js/base.js
+++ youarenotalone/website/static/website/js/base.js
@@ -10,6 +10,7 @@ $(document).ready(function () {
     setTimeout(function(){$(".alert").fadeOut('normal');}, 3000);
 
     //Search users
+    var markerList = new Array();
     $('.searchForm').on('submit', function(event){
         event.preventDefault();
         console.log($('#searchInput').val()) // check value
@@ -30,12 +31,23 @@ $(document).ready(function () {
                         $('#noResult').empty();
                         $('#mapid').css('display', 'block');
                         //Markers
+
+                        //Delete old markers
+                        if (markerList != undefined) {
+                            $.each(markerList, function (index, value) { 
+                                mymap.removeLayer(value);
+                                markerList = new Array();
+                            });
+                        }
+
+                        //Set new markers
                         $.each(data, function (index, value) { 
                             var marker = L.marker([value.Lng, value.Lat]).addTo(mymap);
                             marker.bindPopup("<b>" + value.name + "</b>").openPopup();
+                            markerList.push(marker);
+                            mymap.addLayer(marker);
+                            console.log(markerList);
                         });
-                        
-                        
                     }
                 }
             }
