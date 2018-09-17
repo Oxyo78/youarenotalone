26b924e8743bad0a82bdbbfdc7a4634d4b3cc7cd
Oxyo78 <vienne.yohan@gmail.com>
2018-09-17 21:52:24 +0200

    Add travis.yml config

---
+++ youarenotalone/settings/travis.py
@@ -0,0 +1,11 @@
+from . import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'travis_ci_test',
+        'USER': 'postgres',  
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '5432', }
+}
